4be1c8393558d8c7dede992bcd3ed4a558c04610
wessal-el <wessalelhasnaoui@gmail.com>
2023-03-22 16:31:42 -0700

    yes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
-int -putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-
-
-
-#endif
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
