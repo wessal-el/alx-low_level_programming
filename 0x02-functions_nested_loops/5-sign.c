afc8b34fa69e0c09e8039e43f4007a93cf7a7e7d
wessal-el <wessalelhasnaoui@gmail.com>
2023-03-22 16:51:31 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,34 +1,26 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_sign - Prints the dign of a number
+ * @n: number to check
+ * Return:1 if n is greater than zero,0 if n is zero, -1 if n is less than zero
  */
-int main(void)
+
+int print_sign(int n)
 {
-    int r;
-
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar('0');
+	return (0);
+	}
+	else
+	{
+	_putchar('-');
+	return (-1);
+	}
 }
-
