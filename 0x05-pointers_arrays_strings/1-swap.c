95d5f11902cc8c530b5de9f40708dbaab7a99edf
wessal-el <wessalelhasnaoui@gmail.com>
2023-03-27 14:54:29 -0700

    task123

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: the first integer to be swapped
+ * @b: the second integer to be swapped
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+/* the function that swaps the values of two integers. */
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
